9765d9bfa2d3f5133c4ac4ac52e68e4940b2e85c
Serhii Minin <sergey.minin@brainberry.ua>
2018-10-08 15:59:38 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const App = () => (
+  <div>Hello, World!</div>
+);
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
